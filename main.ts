d769a50e310872a9a136d71a7a1fc0a90c2c0885
Marcel1-MTL <123545925+Marcel1-MTL@users.noreply.github.com>
2023-01-25 16:32:03 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,10 +3,14 @@ input.onPinPressed(TouchPin.P0, function () {
     if (Light_State == 0) {
         Light_State = 3
         basic.showIcon(IconNames.Yes)
+        radio.sendNumber(12)
     } else {
         Light_State = 0
         basic.showIcon(IconNames.No)
     }
+})
+basic.forever(function () {
+    radio.setGroup(88)
 })
 basic.forever(function () {
     if (Light_State == 1) {
